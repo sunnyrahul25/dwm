f8f8f041b516a73baf746bd1a1b9aa8911dd57c4
Sunny Rahul <sunny.rahul@tutanota.com>
2021-02-16 14:12:32 +0100

    fix dwm command

--- config.h
+++ config.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run_history", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun","-monitor","0","-show-icons", NULL };
 static const char *termcmd[]  = {  "st", NULL };
 static const char *capturecmd[]  = {  "emacsclient","-n","-e","(yequake-toggle \"org-capture\")",NULL };
